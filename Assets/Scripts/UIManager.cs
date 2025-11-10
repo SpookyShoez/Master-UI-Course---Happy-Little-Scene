1d9370dfd6cd3229db0d0d92879c92c79cbda308
Morialle Barr <Moriallekbarr@gmail.com>
2025-11-10 14:05:54 +0200

    Lesson 0-6

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnRestartPress()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnGameResumePress()
+    {
+       
+    }
+
+    public void OnGameExitPress()
+    {
+        
+    }
+}
